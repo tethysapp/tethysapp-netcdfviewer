ba8dc4ac76410528626f9e9da46622f6f6345496
jenochjones <jon.enoch.jones@gmail.com>
2020-09-14 15:22:16 -0600

    changed permissions

--- tethysapp/netcdfviewer/timeseries.py
+++ tethysapp/netcdfviewer/timeseries.py
@@ -3,6 +3,8 @@ import xarray
 import geomatics
 import json
 import requests
+import os
+import stat
 
 def get_box_values(request):
     maxlat = float(request.GET['maxlat'])
@@ -13,12 +15,12 @@ def get_box_values(request):
     url = request.GET['odurl']
     var = request.GET['var']
     path_to_netCDF = '/Users/jonjones/tethys-apps/apps/tethysapp-netcdfviewer/tethysapp/netcdfviewer/workspaces/app_workspace/temp.nc'
-
+    os.chmod(path_to_netCDF, stat.S_IWOTH)
     ds = xarray.open_dataset(url)
 
     time_series = ds[var].sel(latitude=slice(maxlat, minlat), longitude=slice(minlon, maxlon))
     time_series.to_netcdf(path=path_to_netCDF)
-    print('type: ' + str(coord))
+
     if not coord is False:
         print('if')
         data = geomatics.timeseries.point([path_to_netCDF], var, (coord[0], coord[1]), dims=('latitude', 'longitude'), t_var='time')
@@ -29,8 +31,6 @@ def get_box_values(request):
         data = geomatics.timeseries.full_array_stats([path_to_netCDF], var, t_var='time')
         time = 'datetime'
         value = 'mean'
-
-    print(data)
 
 
     return JsonResponse({'data': data, 'time': time, 'value': value})
