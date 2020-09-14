8c5d0175ef29cf491295bda716d6be7632cebaf5
jenochjones <jon.enoch.jones@gmail.com>
2020-09-14 15:42:43 -0600

    changed permissions

--- tethysapp/netcdfviewer/timeseries.py
+++ tethysapp/netcdfviewer/timeseries.py
@@ -4,7 +4,7 @@ import geomatics
 import json
 import requests
 import os
-import stat
+
 
 def get_box_values(request):
     maxlat = float(request.GET['maxlat'])
@@ -14,21 +14,20 @@ def get_box_values(request):
     coord = json.loads(request.GET['coord'])
     url = request.GET['odurl']
     var = request.GET['var']
-    path_to_netCDF = '/Users/jonjones/tethys-apps/apps/tethysapp-netcdfviewer/tethysapp/netcdfviewer/workspaces/app_workspace/temp.nc'
-    os.chmod(path_to_netCDF, stat.S_IWOTH)
+    path_to_netcdf = os.path.join(os.path.dirname(__file__), 'workspaces', 'app_workspace', 'temp.nc')
     ds = xarray.open_dataset(url)
 
     time_series = ds[var].sel(latitude=slice(maxlat, minlat), longitude=slice(minlon, maxlon))
-    time_series.to_netcdf(path=path_to_netCDF)
+    time_series.to_netcdf(path=path_to_netcdf)
 
     if not coord is False:
         print('if')
-        data = geomatics.timeseries.point([path_to_netCDF], var, (coord[0], coord[1]), dims=('latitude', 'longitude'), t_var='time')
+        data = geomatics.timeseries.point([path_to_netcdf], var, (coord[0], coord[1]), dims=('latitude', 'longitude'), t_var='time')
         time = 'datetime'
         value = 'values'
     else:
         print('then')
-        data = geomatics.timeseries.full_array_stats([path_to_netCDF], var, t_var='time')
+        data = geomatics.timeseries.full_array_stats([path_to_netcdf], var, t_var='time')
         time = 'datetime'
         value = 'mean'
 
