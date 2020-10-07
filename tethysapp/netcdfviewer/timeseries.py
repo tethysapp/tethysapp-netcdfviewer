09b6d7dcba8589e2f0247c9dcad03bf08b24dd95
jenochjones <jon.enoch.jones@gmail.com>
2020-10-07 10:25:01 -0600

    Updated

--- tethysapp/netcdfviewer/timeseries.py
+++ tethysapp/netcdfviewer/timeseries.py
@@ -8,29 +8,15 @@ import urllib
 
 
 def get_box_values(request):
-    maxlat = float(request.GET['maxlat'])
-    maxlon = float(request.GET['maxlng'])
-    minlat = float(request.GET['minlat'])
-    minlon = float(request.GET['minlng'])
     lat = request.GET['lat']
     lon = request.GET['lon']
     time = request.GET['time']
-    url = request.GET['odurl']
     subset_url = request.GET['subsetURL']
     var = request.GET['var']
     coord = json.loads(request.GET['coord'])
     path_to_netcdf = os.path.join(os.path.dirname(__file__), 'workspaces', 'app_workspace', 'temp.nc')
 
-    print(coord)
-    #try:
-    print(json.loads(subset_url))
     urllib.request.urlretrieve(json.loads(subset_url), path_to_netcdf)
-    #except:
-    #    print('except!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    #    var_dict = {lat: slice(maxlat, minlat), lon: slice(minlon, maxlon)}
-    #    ds = xarray.open_dataset(url)
-    #    time_series = ds[var].sel(var_dict)
-    #    time_series.to_netcdf(path=path_to_netcdf)
 
     if coord is not False:
         data = geomatics.timeseries.point([path_to_netcdf], var, (coord[0], coord[1]), (lat, lon), time)
