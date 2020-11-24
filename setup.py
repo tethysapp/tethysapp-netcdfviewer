5119ea0caebaef4bfa512c5b6d7cbe640b024513
jenochjones <jon.enoch.jones@gmail.com>
2020-11-24 13:14:46 -0700

    updated version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces'
 
 setup(
     name=release_package,
-    version='0.0.8',
+    version='0.0.9',
     description='An app to view netCDF files hosted on a THREDDS Data Server',
     long_description='',
     keywords='netCDF',
